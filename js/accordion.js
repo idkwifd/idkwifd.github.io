480b1f9d19a4aa596c9c2fe6aa803fba17e21b3b
idkwifd <thibault.meyrat@gmail.com>
2018-02-04 13:25:09 -0800

    TestAcc

--- js/accordion.js
+++ js/accordion.js
@@ -12,18 +12,19 @@ $(function() {
   // console.log(hash);
   var active = false;
   // Activate appropriate accordion item and scroll to it:
+  window.alert("1");
   if (hash.slice(0,3) === 'pub') {
+    window.alert("2");
     active = 0;
   } else if (hash === 'cv' | hash === 'curriculum_vitae' | hash === 'vitae') {
     active = 1;
+    window.alert("3");
   }
   if (hash) {
     $('#accordion').accordion('option', 'active', active);
     $('html,body').animate({
       scrollTop: $('#accordion').offset().top
     });
+    window.alert("4");
   }
-
-  
-
 });
